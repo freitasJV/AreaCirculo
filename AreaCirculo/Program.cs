637a775e1f729ac9456808841f99a6582aac8ca2
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:11:38 -0300

    Add project files.

---
+++ AreaCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, pi = 3.14159;
+
+            Console.WriteLine("Digite o valor do raio do círculo:");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A = {area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}

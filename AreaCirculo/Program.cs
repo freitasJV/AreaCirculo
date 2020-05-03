ed1a2495b665480bc984b8438da0819837b55a8c
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:21:18 -0300

    Adicionando tratamento de excessões

--- AreaCirculo/Program.cs
+++ AreaCirculo/Program.cs
@@ -7,14 +7,32 @@ namespace AreaCirculo
     {
         static void Main(string[] args)
         {
-            double raio, area, pi = 3.14159;
+            try
+            {
+                double raio, area, pi = 3.14159;
 
-            Console.WriteLine("Digite o valor do raio do círculo:");
-            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Digite o valor do raio do círculo:");
+                raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            area = pi * Math.Pow(raio, 2);
+                if (raio < 0)
+                {
+                    Console.WriteLine("Valor inválido");
+                }
+                else
+                {
+                    area = pi * Math.Pow(raio, 2);
 
-            Console.WriteLine($"A = {area.ToString("F4", CultureInfo.InvariantCulture)}");
+                    Console.WriteLine($"A = {area.ToString("F4", CultureInfo.InvariantCulture)}");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
